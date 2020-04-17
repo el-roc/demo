efa32624cdabb0cbef77af4f0e9e6e3de58cf3a1
el-roc <eric.eespinoza@gmail.com>
2020-04-17 10:04:00 -0400

    new map style editt

--- app/routes.js
+++ app/routes.js
@@ -336,7 +336,7 @@ app.put('/keepPark', (req, res) => {
 
         // process the login form
         app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
+            successRedirect : '/map', // redirect to the secure profile section
             failureRedirect : '/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }));
